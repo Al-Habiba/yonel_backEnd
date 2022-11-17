e679649ebb74cf882dbe1878093b1def8fbbe386
user <sangoule.ndao@uadb.edu.sn>
2022-11-17 01:32:25 +0000

    configuration reussi

--- models/index.js
+++ models/index.js
@@ -1,19 +1,12 @@
-const dbConfig=require("../config/db.conf");
+const config=require("../config/config.json");
 
 const Sequelize= require("sequelize");
 
-const sequelize= new Sequelize (dbConfig.DB, dbConfig.USER,dbConfig.PASSWORD,{
-    host:dbConfig.HOST,
-    dialect:dbConfig.dialect,
-    operatorsAliases:false,
-    pool: 
-    {
-        max:dbConfig.pool.max,
-        min:dbConfig.pool.min,
-        acquire:dbConfig.pool.acquire,
-        idle:dbConfig.pool.idle
-    }
-})
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    "port":8880,
+    dialect:'mysql'
+  });
 
 const db={}
 
--- server.js
+++ server.js
@@ -1,22 +1,12 @@
-const express =require('express');
-const app=express();
-const bodyParser=require("body-parser");
-const cors=require("cors");
-PORT = process.env.PORT || 8780;
-
-const corsOptions={
-    origin:"http://localhost:8780"
-}
-
-app.use(cors(corsOptions))
-
-app.use(bodyParser.json());
-
-
-var db=require("./models/index");
-db.sequelize.sync({force: true})
-.then(()=>{console.log("Base de données bien synchronisée")})
-.catch((err=>{console.log("Echec lors de la synchronisation :"+err.message)}))
+const express = require("express");
+const app = express();
+var corsOptions = {
+    origin: "http://localhost:8889" // URL of the frontend
+    };
+app.use(express.json()); // parsing application/json
+app.use(express.urlencoded({ extended: true })); // parsing application/x-www-form-urlencodedconst  // 
+const db = require("./models/index.js");
+PORT = process.env.PORT || 8880;
 app.listen(PORT, () => {
     console.log(`Serveur lancé sur localhost:${PORT}`);
  });
